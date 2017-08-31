e49deae8c713cbc56e99e56d3f0a865ab86eb427
salliyme <307623183@qq.com>
2017-08-31 21:55:50 +0800

    backend， 微信authclient

--- frontend/components/AuthHandler.php
+++ frontend/components/AuthHandler.php
@@ -6,6 +6,7 @@
  */
 
 namespace frontend\components;
+use common\models\WechatUser;
 use Yii;
 use yii\authclient\ClientInterface;
 use yii\helpers\ArrayHelper;
@@ -28,10 +29,38 @@ class AuthHandler
     public function handle()
     {
         $attributes = $this->client->getUserAttributes();
-        $email = ArrayHelper::getValue($attributes, 'email');
         $openid = ArrayHelper::getValue($attributes, 'openid');
-        $nickname = ArrayHelper::getValue($attributes, 'nickname');
 
+        /* @var $weUser WechatUser */
+        $weUser = WechatUser::findOne(['openid' => $openid]);
+        if ($weUser) {
+            // update
+            $weUser->openid = $attributes['openid'];
+            $weUser->nickname = $attributes['nickname'];
+            $weUser->sex = $attributes['sex'];
+            $weUser->headimgurl = $attributes['headimgurl'];
+            $weUser->country = $attributes['country'];
+            $weUser->province = $attributes['province'];
+            $weUser->city = $attributes['city'];
+            $weUser->access_token = $this->client->getAccessToken()->getToken();
+            $weUser->refresh_token = $this->client->getAccessToken()->getParam('refresh_token');
+            $weUser->created_at = $_SERVER['REQUEST_TIME'];
+            $weUser->save();
+        } else {
+            // add
+            $weUser = new WechatUser();
+            $weUser->openid = $attributes['openid'];
+            $weUser->nickname = $attributes['nickname'];
+            $weUser->sex = $attributes['sex'];
+            $weUser->headimgurl = $attributes['headimgurl'];
+            $weUser->country = $attributes['country'];
+            $weUser->province = $attributes['province'];
+            $weUser->city = $attributes['city'];
+            $weUser->access_token = $this->client->getAccessToken()->getToken();
+            $weUser->refresh_token = $this->client->getAccessToken()->getParam('refresh_token');
+            $weUser->created_at = $_SERVER['REQUEST_TIME'];
+            $weUser->save();
+        }
         if (Yii::$app->user->isGuest) {// 如果是未登录账号就处理添加账号
 
         } else { // 已登录用户就与威信号绑定
